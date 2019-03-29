81f8da5190a2fe8e0b049f3f56b7ff93e9a3d6d1
Alex Morin <alex.a.morin@usherbrooke.ca>
2019-03-29 14:39:46 -0400

    Added progressbar, fixed JSON report, added /log endpoint

--- scripts/disableAP.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo systemctl stop hostapd
-sudo systemctl stop dnsmasq
-echo -n "Ok"
-
--- scripts/enableAP.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo systemctl start hostapd
-sudo systemctl start dnsmasq
-echo -n "Ok"
